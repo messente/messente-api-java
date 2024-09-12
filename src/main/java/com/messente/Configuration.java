f836539ce14b5e8a765734779e6bfde7631f5458
api-librarian <development@messente.com>
2024-09-12 14:50:37 +0000

    Travis update: Sep 2024 (Build 797)
    [skip ci]

--- src/main/java/com/messente/Configuration.java
+++ src/main/java/com/messente/Configuration.java
@@ -15,7 +15,7 @@ package com.messente;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.6.0")
 public class Configuration {
-    public static final String VERSION = "4.2.1";
+    public static final String VERSION = "4.3.0";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
