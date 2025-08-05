cad51d21db44f029a3159069ae3ae99e31d8c847
api-librarian <development@messente.com>
2025-08-05 08:19:08 +0000

    Travis update: Aug 2025 (Build 953)
    [skip ci]

--- src/main/java/com/messente/ApiClient.java
+++ src/main/java/com/messente/ApiClient.java
@@ -1,6 +1,6 @@
 /*
  * Messente API
- * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber and WhatsApp messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
+ * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
  *
  * The version of the OpenAPI document: 2.0.0
  * Contact: messente@messente.com
@@ -145,7 +145,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/4.5.2/java");
+        setUserAgent("OpenAPI-Generator/4.5.0/java");
 
         authentications = new HashMap<String, Authentication>();
     }
